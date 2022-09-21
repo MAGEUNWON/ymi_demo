f70150e125b17524dbc060bf5492d8a415ab6f86
unknown <geunwon1947@gmail.com>
2022-09-21 18:12:35 +0900

    모달창을 만들자

---
+++ information/information.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+const password = document.getElementById('password');
+const psConfirm = document.getElementById('psConfirm');
+const nickname = document.getElementById('nickname');
+const submit = document.getElementById('submit');
+const button = document.getElementById('button');
+
